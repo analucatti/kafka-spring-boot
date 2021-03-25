1ebf88a1e7311e48e1f3c0253d6eb515dcc965eb
Ana Lucatti <analucatti23@gmail.com>
2021-03-25 19:08:58 -0300

    Added exception handler

--- src/main/java/com/victorromano/kafkaspringboot/controller/SampleRestController.java
+++ src/main/java/com/victorromano/kafkaspringboot/controller/SampleRestController.java
@@ -2,6 +2,8 @@ package com.victorromano.kafkaspringboot.controller;
 
 import com.victorromano.kafkaspringboot.kafka.SampleKafkaProducer;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,14 +21,13 @@ public class SampleRestController {
     }
 
     @PostMapping("/sample")
-    public String sample() {
-        String uuid = UUID.randomUUID().toString();
-        return producer.send(uuid, uuid);
+    public ResponseEntity<String> sample() {
+        return ResponseEntity.status(HttpStatus.OK).body(producer.send(UUID.randomUUID().toString(), Long.toString(System.currentTimeMillis())));
     }
 
     @PostMapping("/message")
-    public String message(@RequestBody MessageDto messageDto) {
-        return producer.send(UUID.randomUUID().toString(), messageDto.getMessage());
+    public ResponseEntity<String> message(@RequestBody MessageDto messageDto) {
+        return ResponseEntity.status(HttpStatus.OK).body(producer.send(UUID.randomUUID().toString(), messageDto.getMessage()));
     }
 
 }
