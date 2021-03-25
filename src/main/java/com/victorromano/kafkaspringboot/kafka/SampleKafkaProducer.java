ea81e86eeb9b01c51203b8532a3fbc694d5247d3
Ana Lucatti <analucatti23@gmail.com>
2021-03-25 18:26:21 -0300

    Updated consumer and producer to use topic name from properties

--- src/main/java/com/victorromano/kafkaspringboot/kafka/SampleKafkaProducer.java
+++ src/main/java/com/victorromano/kafkaspringboot/kafka/SampleKafkaProducer.java
@@ -8,7 +8,11 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class SampleKafkaProducer {
-    private KafkaTemplate<Key, Value>  kafkaTemplate;
+
+    private KafkaTemplate<Key, Value> kafkaTemplate;
+
+    @org.springframework.beans.factory.annotation.Value("${kafka.topic}")
+    private String topicName;
 
     @Autowired
     public SampleKafkaProducer(KafkaTemplate<Key, Value> kafkaTemplate) {
@@ -17,14 +21,14 @@ public class SampleKafkaProducer {
 
     public String send(String key, String message) {
         Key avroKey = Key.newBuilder()
-                .setKey(key)
-                .build();
+            .setKey(key)
+            .build();
 
         Value avroMessage = Value.newBuilder()
-                .setMessage(message)
-                .build();
+            .setMessage(message)
+            .build();
 
-        kafkaTemplate.send("myTopic2", avroKey, avroMessage);
+        kafkaTemplate.send(topicName, avroKey, avroMessage);
         return message;
     }
 
